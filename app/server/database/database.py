361db19f929e148ee65ccce4cef6bf499c6f59b7
biggvladik <zuevvlad55@gmail.com>
2023-07-04 01:11:09 +0300

    Database готова

--- app/server/database/database.py
+++ app/server/database/database.py
@@ -1,26 +1,34 @@
-from sqlalchemy import create_engine,String,DateTime,Column,Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import  sessionmaker
-from sqlalchemy_utils import database_exists, create_database
-
-from config import DB_PASSWORD, DB_NAME
+from pymongo import MongoClient
 
 
-database_url = f"postgresql+psycopg2://postgres:{DB_PASSWORD}@localhost/{DB_NAME}"
-engine=create_engine(database_url)
+class Database:
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["test"]
+        self.collection = self.db["test"]
 
-Session = sessionmaker(engine)
-Base = declarative_base()
-
-if not database_exists(engine.url):
-    create_database(engine.url)
-
-
-class Users(Base):
-    __tablename__ = 'Users'
-
-
+    def get_tokens(self,id:str,count:int):
+        res = self.collection.find_one({'id':id})
+        if res == None:
+            self.collection.insert_one({'money':count,'id':id})
+        else:
+            self.collection.update_one({'id':id}, {'$set':{'money':res['money']+count}})
 
 
-class Transactions(Base):
-    __tablename__ = 'Transactions'
+    def send_tokens(self,id:str,count:int):
+        res = self.collection.find_one({'id': id})
+        if res == None:
+            return False
+        else:
+            if res['money']-count < 0:
+                return False
+            self.collection.update_one({'id': id}, {'$set': {'money': res['money'] - count}})
+            return True
+
+
+    def  send_tokens_to_user(self,adress:str, to_adress:str,count:int):
+        res = self.send_tokens(adress,count)
+        if res:
+            self.get_tokens(to_adress,count)
+
+
